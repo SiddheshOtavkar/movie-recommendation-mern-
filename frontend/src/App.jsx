fe3a510dd463989af37cafca0e7bb8f2002a8ffa
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-10-05 14:38:44 +0530

    last commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,25 +37,10 @@ function App() {
             <div className="container">
                 <div className="head">
                     <img src="./images/logo.png" alt="logo" className="logo" />
-                    <Search setSearch={(search) => setSearch(search)} />
                 </div>
                 <div className="body">
                     <div className="table_container">
                         <Table movies={obj.movies ? obj.movies : []} />
-                        <Pagination
-                            page={page}
-                            limit={obj.limit ? obj.limit : 0}
-                            total={obj.total ? obj.total : 0}
-                            setPage={(page) => setPage(page)}
-                        />
-                    </div>
-                    <div className="filter_container">
-                        <Sort sort={sort} setSort={(sort) => setSort(sort)} />
-                        <Genre
-                            filterGenre={filterGenre}
-                            genres={obj.genres ? obj.genres : []}
-                            setFilterGenre={(genre) => setFilterGenre(genre)}
-                        />
                     </div>
                 </div>
             </div>
